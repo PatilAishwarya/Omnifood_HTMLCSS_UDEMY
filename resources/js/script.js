d8f99eb0ffdc8b44f7e0faf2345919a73b304d97
Aishwarya Patil <aish14874@gmail.com>
2020-07-14 18:01:12 +0530

    STICKY NAVIGATION USING WAYPOINTS jS

---
+++ resources/js/script.js
@@ -0,0 +1,12 @@
+/* global document */
+$(document).ready(function () {
+   $('.js--section-features').waypoint(function (direction) {
+       if(direction == 'down'){
+           $('nav').addClass('sticky');
+       }else{
+           $('nav').removeClass('sticky');
+       }
+   }, {
+      offset: '5%'
+   });
+});
